10779fc1705b1a388647930a1b63c1557b9354e3
marianax-magalhaes <marianaximenes@poli.ufrj.br>
2022-01-11 14:02:49 -0300

    perfil teste corrigido

--- src/main/java/com/livrariaapi/service/DbService.java
+++ src/main/java/com/livrariaapi/service/DbService.java
@@ -5,9 +5,11 @@ import com.livrariaapi.domain.Livro;
 import com.livrariaapi.repository.CategoriaRepository;
 import com.livrariaapi.repository.LivroRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.Arrays;
 
+@Service
 public class DbService {
 
     @Autowired
