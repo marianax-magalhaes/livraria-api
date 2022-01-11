81b886d1f7bdde558320abdc1bb83fd6d7ebf3a4
marianax-magalhaes <marianaximenes@poli.ufrj.br>
2022-01-11 12:10:16 -0300

    primeiro commit

---
+++ src/test/java/com/livrariaapi/LivrariaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.livrariaapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LivrariaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
