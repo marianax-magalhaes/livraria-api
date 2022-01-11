b3d593e1246c38540c2e68233e785dbc518f59a5
marianax-magalhaes <marianaximenes@poli.ufrj.br>
2022-01-11 14:29:16 -0300

    perfil dev

---
+++ src/main/java/com/livrariaapi/config/DevConfig.java
@@ -0,0 +1,21 @@
+package com.livrariaapi.config;
+
+import com.livrariaapi.service.DbService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Configuration
+@Profile("dev")
+public class DevConfig {
+
+    @Autowired
+    private DbService dbService;
+
+    @Bean
+    public void instanciaDb() {
+        this.dbService.instanciaDb();
+    }
+}
+
