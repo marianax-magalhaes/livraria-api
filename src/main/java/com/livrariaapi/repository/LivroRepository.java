a3d0a05c6ebda13450f73dd4fdd0f7a1711f9e60
marianax-magalhaes <marianaximenes@poli.ufrj.br>
2022-01-11 12:24:26 -0300

    criacao dos repositorios

---
+++ src/main/java/com/livrariaapi/repository/LivroRepository.java
@@ -0,0 +1,9 @@
+package com.livrariaapi.repository;
+
+import com.livrariaapi.domain.Livro;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface LivroRepository extends JpaRepository<Livro, Integer> {
+}
