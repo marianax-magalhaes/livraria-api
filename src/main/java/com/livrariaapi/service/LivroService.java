1a173e0444fbd4a340b4db61aa0fbe5ebbe2ccab
marianax-magalhaes <marianaximenes@poli.ufrj.br>
2022-01-24 14:16:12 -0300

    + ajustes para put livros

--- src/main/java/com/livrariaapi/service/LivroService.java
+++ src/main/java/com/livrariaapi/service/LivroService.java
@@ -38,6 +38,13 @@ public class LivroService {
     }
 
     private void updateData(Livro newObj, Livro obj) {
+        newObj.setTitulo(obj.getTitulo());
+        newObj.setNome_autor(obj.getNome_autor());
+        newObj.setTexto(obj.getTexto());
+    }
+
+//    para p patch
+    private void updateData1(Livro newObj, Livro obj) {
         if(newObj.getTitulo()==null)newObj.setTitulo(obj.getTitulo());
         if(newObj.getNome_autor()==null)newObj.setNome_autor(obj.getNome_autor());
         if(newObj.getTexto()==null)newObj.setTexto(obj.getTexto());
