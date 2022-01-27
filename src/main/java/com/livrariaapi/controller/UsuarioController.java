dc4a9d448db32bc6f9a14ab8ac682d4821dd8630
marianax-magalhaes <marianaximenes@poli.ufrj.br>
2022-01-27 14:17:31 -0300

    ajustes

--- src/main/java/com/livrariaapi/controller/UsuarioController.java
+++ src/main/java/com/livrariaapi/controller/UsuarioController.java
@@ -3,12 +3,10 @@ package com.livrariaapi.controller;
 import com.livrariaapi.domain.Usuario;
 import com.livrariaapi.service.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@CrossOrigin("*")
 public class UsuarioController {
 
     @Autowired
